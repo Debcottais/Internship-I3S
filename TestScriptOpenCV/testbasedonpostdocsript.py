30577e5fee75302b27ea91692e785a26471abc0f
Debcottais <36454186+Debcottais@users.noreply.github.com>
2019-03-26 14:38:16 +0100

    Add files via upload

--- TestScriptOpenCV/testbasedonpostdocsript.py
+++ TestScriptOpenCV/testbasedonpostdocsript.py
@@ -3,8 +3,31 @@ import cv2
 import matplotlib.pyplot as plt
 import matplotlib as mpl 
 import imutils
+from scipy.cluster import hierarchy
+from random import randint
 
 cap=cv2.VideoCapture('La cellule en mouvement.avi')
+
+######################################
+### bounding box part 
+
+# Specify the tracker type
+trackerName='crst' ## Discriminative correlation filter tracker with channel and spatial reliability
+
+# Create MultiTracker object 
+trackers=cv2.MultiTracker_create()
+OPENCV_OBJECT_TRACKERS = {
+    "csrt": cv2.TrackerCSRT_create,
+    "kcf": cv2.TrackerKCF_create,
+    "boosting": cv2.TrackerBoosting_create,
+    "mil": cv2.TrackerMIL_create,
+    "tld": cv2.TrackerTLD_create,
+    "medianflow": cv2.TrackerMedianFlow_create,
+    "mosse": cv2.TrackerMOSSE_create
+}
+
+#######################################
+#### first step of the post doc script 
 
 ## background substraction 
 fgbg=cv2.bgsegm.createBackgroundSubtractorMOG()
@@ -24,30 +47,90 @@ while(True):
 
 ## erosion
     kernel=np.ones((5,5),np.uint8) #uint8==>8 bit integer (represent values between 0 to 255)
-    erosion=cv2.erode(frame,kernel,iterations=1) #a voir si erosion surframe de depart ou sur frame apres filtre soit sur th3
+    erosion=cv2.erode(th3,kernel,iterations=1) #a voir si erosion surframe de depart ou sur frame apres filtre soit sur th3
 
-    # cv2.imshow('erosion',erosion)
+    cv2.imshow('erosion',erosion)
 ## ou a la place de erosion : on fait un opening pour enlever les pixels qui pourrait etre en trop et donc confondus avec une cell
     kernel=np.ones((5,5),np.uint8) #uint8==>8 bit integer (represent values between 0 to 255)
     opening=cv2.morphologyEx(frame,cv2.MORPH_OPEN,kernel)
 
-    cv2.imshow('opening',opening)
+    # cv2.imshow('opening',opening)
 
 ## ou closing 
     kernel=np.ones((5,5),np.uint8) #uint8==>8 bit integer (represent values between 0 to 255)
     closing=cv2.morphologyEx(frame,cv2.MORPH_CLOSE,kernel)
 
-    cv2.imshow('closing',closing)
+    # cv2.imshow('closing',closing)
     cv2.imshow('cap',frame)
+
+##############################################################
+###### suite of bounding box part 
+
+## draw bounding box for selection of ROI
+
+    success, boxes=trackers.update(frame)
+
+# loop over the bounding boxes and draw them on the frame
+    for box in boxes:
+        (x, y, w, h) = [int(v) for v in box]
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(0) & 0xFF
+
+    # if the 's' key is selected, we are going to "select" a bounding
+    # box to track
+    if key == ord("s"):
+        colors = []
+    # select the bounding box of the object we want to track (make
+    # sure you press ENTER or SPACE after selecting the ROI)
+        box = cv2.selectROIs("Frame", frame, fromCenter=False, showCrosshair=True)
+        box = tuple(map(tuple, box)) 
+        for bb in box:
+            tracker = OPENCV_OBJECT_TRACKERS[trackerName]()
+            trackers.add(tracker, frame, bb)
+
+# if you want to reset bounding box, select the 'r' key 
+    elif key == ord("r"):
+        trackers.clear()
+        trackers = cv2.MultiTracker_create()
+
+        box = cv2.selectROIs("Frame", frame, fromCenter=False, showCrosshair=True)
+        box = tuple(map(tuple, box))
+        for bb in box:
+            tracker = OPENCV_OBJECT_TRACKERS[trackerName]()
+            trackers.add(tracker, frame, bb)
+
+    elif key == ord("q"):
+        break
+        # sys.exit(1)
+
+    while cap.isOpened():
+        success,frame=cap.read()
+        if not success:
+            break
+        success,boxes=trackers.update(frame)
+        
+        for i, newbox in enumerate(boxes):
+            p1=(int(newbox[0]), int(newbox[1]))
+            p2=(int(newbox[0]+newbox[2]),int(newbox[1]+newbox[3]))
+            cv2.rectangle(frame,p1,p2,colors[i],2,1)
+        cv2.imshow('Multitracker',frame)
+
+
+    ##################################################
+    #### suite of the script post doc part 
 
     k=cv2.waitKey(50) & 0xff #plus on augmente la key plus la video va doucement
     if k==47:
         break
+    
 
 cap.release()
 cv2.destroyAllWindows()
 
-    ################################################
+
+################################################
 ########### avant ca tout marche ##################
 #####################################################
 
@@ -77,4 +160,22 @@ cv2.destroyAllWindows()
 
     # cv2.imshow('threshold',filtered)
 
+## find contours in the thresholded image 
+    # ret,contours,hierarchy=cv2.findContours(th3,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    # cnts=contours[0]
+    # # cnts=imutils.grap_contours(cnts)
+    # for cnts in contours:
+    #     M=cv2.moments(cnts)
+    #     if M["m00"] != 0:
+    #         cX = int(M["m10"] / M["m00"])
+    #         cY = int(M["m01"] / M["m00"])
+    #     else:
+    #         cX, cY = 0, 0
+    #     cX=int(M["m10"]/M["m00"])
+    #     cY=int(M["m01"]/M["m00"])
+    # # draw the contour and center of the shape on the image
+    # # cv2.drawContours(frame, [c], -1, (0, 255, 0), 2)
+    #     cv2.circle(frame, (cX, cY), 7, (255, 255, 255), -1)
+    #     cv2.putText(frame, "centroid", (cX - 20, cY - 20),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
+    #     cv2.imshow("centroid",frame)
