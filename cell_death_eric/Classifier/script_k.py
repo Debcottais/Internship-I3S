74257a5a76fee36ed591a634fce98c7ef9ac806f
Debcottais <36454186+Debcottais@users.noreply.github.com>
2019-07-17 09:55:21 +0200

    Create script_k.py

---
+++ cell_death_eric/Classifier/script_k.py
@@ -0,0 +1,4 @@
+### K-Nearest Neighbors operates by checking the distance from some test 
+## example to the known values of some training example. The group of data 
+## points/class that would give the smallest distance between the training 
+## points and the testing point is the class that is selected.
